a4afd2a3f530e4b2fffd7d021f3df9240ae0be40
alphanumeric-username <geometrianaoeuclidiana@gmail.com>
2026-05-05 14:30:52 -0300

    add: explanatory comments to headers

--- fwd-node.sh
+++ fwd-node.sh
@@ -3,16 +3,10 @@
 module load anaconda3/2022.10
 module load openmpi/4.0.7-gcc-ucx-1.13.1
 
-#conda init bash
 source ~/.bashrc
 
-# source activate devito
 conda deactivate
 conda activate pylops
-# conda activate pylops_mpi_clone
 cd $CWD
 
-# export DEVITO_LANGUAGE=openmp
-
-# mpirun -np 5 
 mpirun python fwd.py $@
